276f9fbae6b10806f0dd3620214d403c08273019
xubaoshi <948869516@qq.com>
2018-03-28 11:10:32 +0800

    edit mock server code

--- src/service/autoTest.js
+++ src/service/autoTest.js
@@ -3,29 +3,43 @@ const debug = require('debug')('auto test')
 
 const { isEmpty } = require('../utils/lang')
 
-function testUrl(url, ticket) {
-  return new Promise((resolve, reject) => {
-    const options = {
-      method: 'POST',
-      url,
-      headers: {
-        ticket,
-        // 'User-Agent': 'request',
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json',
-      }
+const privateFn = {
+  mockRequestArgs(requestArgs) {
+    const result = {}
+    const args = JSON.parse(requestArgs)
+    if (!isEmpty(args)) {
+      args.forEach(arg => {
+        result[arg.name] = privateFn.mockDataByType(arg.type)
+      })
     }
-    request(options, (error, response, body) => {
-      // 后端内部统一 code为0时请求成功
-      if (!error && response.code === 0) {
-        debug(JSON.stringify(body))
-        resolve(true)
-      } else {
-        debug(JSON.stringify(error))
-        reject(false)
+    return result
+  },
+  mockDataByType() {
+    return 'mock'
+  },
+  testUrl(url, ticket) {
+    return new Promise((resolve, reject) => {
+      const options = {
+        method: 'POST',
+        url,
+        headers: {
+          ticket,
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json',
+        }
       }
+      request(options, (error, response, body) => {
+        // 后端内部统一 code为0时请求成功
+        if (!error && response.code === 0) {
+          debug(JSON.stringify(body))
+          resolve(true)
+        } else {
+          debug(JSON.stringify(error))
+          reject(false)
+        }
+      })
     })
-  })
+  }
 }
 
 async function autoTest(apiList, site, ticket) {
@@ -39,13 +53,26 @@ async function autoTest(apiList, site, ticket) {
     module.folders.forEach((folder, j) => {
       folder.children.forEach((child, k) => {
         const url = site + child.url.replace('$prefix$', '')
-        requestArr.push({ url, moduleIndex: i, folderIndex: j, childIndex: k })
+        const args = JSON.parse(child.requestArgs)
+        // let requestArgs = {}
+        // if (!isEmpty(args)) {
+        //   args.forEach(arg => {
+        //     requestArgs[arg.name] = privateFn.mockDataByType(arg.type)
+
+        //   })
+        // }
+        requestArr.push({
+          url,
+          moduleIndex: i,
+          folderIndex: j,
+          childIndex: k,
+          requestArgs: privateFn.mockRequestArgs(child.requestArgs)
+        })
       })
     })
   })
-  const promises = requestArr.map(data => testUrl(data.url))
+  const promises = requestArr.map(data => privateFn.testUrl(data.url))
   const results = await Promise.all(promises)
-  console.log(results)
   return result
 }
 module.exports = autoTest
